05622b87c76496bb0c8f387700a5c674cc366da4
DonizeteVida <donizetevida1998@outlook.com>
2024-06-24 00:10:11 -0300

    Adding json parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, fs::File};
+
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct JsonData {
+    libraries: Vec<String>,
+    relations: HashMap<u32, Vec<u32>>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let file = File::open("sample/generated.json")?;
+    let json_data: JsonData = serde_json::from_reader(file)?;
+    println!("{:?}", json_data);
+    Ok(())
 }
